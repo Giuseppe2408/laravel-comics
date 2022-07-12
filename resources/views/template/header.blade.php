60cd548bae032b9306ec7b171fcc941ed80ca42c
Giuseppe <giuseppe.acito.ga@gmail.com>
2022-07-12 15:28:43 +0200

    first commit

---
+++ resources/views/template/header.blade.php
@@ -0,0 +1,34 @@
+<header>
+    <img src={{asset("dc-logo.png")}} alt="logo dc comics">
+      <nav>
+          <ul>
+              <li>
+                <a href="">characters</a>
+              </li>
+              <li>
+                <a href="">comics</a>
+              </li>
+              <li>
+                <a href="">movies</a>
+              </li>
+              <li>
+                <a href="">tv</a>
+              </li>
+              <li>
+                <a href="">games</a>
+              </li>
+              <li>
+                <a href="">collectibles</a>
+              </li>
+              <li>
+                <a href="">fans</a>
+              </li>
+              <li>
+                <a href="">News</a>
+              </li>
+              <li>
+                <a href="">Shop</a>
+              </li>
+          </ul>
+      </nav>
+</header>
